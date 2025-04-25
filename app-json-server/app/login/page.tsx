73aed5bd11ceccfbf589e7eb98351a874019b398
Lucas2000-student <rm560179@fiap.com.br>
2025-04-25 19:30:22 -0300

    Não sei o qe mudou

--- app-json-server/app/login/page.tsx
+++ app-json-server/app/login/page.tsx
@@ -2,49 +2,49 @@
 
 import api from "@/services/api"
 import Link from "next/link"
-import { Router, useRouter } from "next/router"
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
-interface Usuario{
-    id:number
-    nome:string
-    usuario:string
-    senha:string
-    status:string
+interface Usuario {
+    id: number
+    nome: string
+    usuario: string
+    senha: string
+    status: string
 }
 
-export default function login(){
-    const[usuarios,setUsuarios] = useState<Usuario[]>([]);
+export default function login() {
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
     const router = useRouter()
-    function carregarUsuarios(){
+    function carregarUsuarios() {
         api.get("/login")
-        .then(function(resposta){
-            setUsuarios(resposta.data);
-        })
+            .then(function (resposta) {
+                setUsuarios(resposta.data);
+            })
     }
-    useEffect(function(){
+    useEffect(function () {
         carregarUsuarios();
-    },[])
-    return(
+    }, [])
+    return (
         <div>
             <h1>Lista de Usuários!</h1>
             <ul>
                 {usuarios.map(
-                    function(usuario){
-                        return(
-                            <li key={usuario.id} onClick={function(){
+                    function (usuario) {
+                        return (
+                            <li key={usuario.id} onClick={function () {
                                 router.push(`/login/${usuario.id}`)
                             }}>
                                 <p><strong>Nome: </strong>{usuario.nome}</p>
                                 <p><strong>Usuário: </strong>{usuario.usuario}</p>
                                 <p><strong>Status: </strong>{usuario.status}</p>
-                                <hr/>
+                                <hr />
                             </li>
                         )
                     }
                 )}
             </ul>
-            <br/>
+            <br />
             <Link href={"/"}>Tela de início</Link>
         </div>
     )
